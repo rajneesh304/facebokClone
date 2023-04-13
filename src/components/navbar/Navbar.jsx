7cc5f691d0a1df281ed80617ccfc700bfce612cc
rajneesh304 <singhatiya.rajneesh2@gmail.com>
2023-04-13 22:48:07 +0530

    implemented leftbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import "./navbar.scss"
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>Fakebook</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="search" />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlineOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+          <span>John Doe</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
