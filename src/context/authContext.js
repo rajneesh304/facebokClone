81803fdc9dc8bce075326b3182e8d690f15b1973
rajneesh304 <singhatiya.rajneesh2@gmail.com>
2023-05-08 17:47:50 +0530

    synched auth with backend

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,13 +1,17 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
+  const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user") == "undefined" ? null : localStorage.getItem("user")) || null);
 
-  const login = () => {
-    //
-    setCurrentUser({ id: 1, name: "John DOe", profilePic: "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" });
+  const login = async (inputs) => {
+    const res = await axios.post("http://localhost:8800/api/auth/login", inputs, {
+      withCredentials: true,
+    });
+
+    setCurrentUser(res.data);
   };
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(currentUser));
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,37 +1,60 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import "./login.scss"
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
 
 const Login = () => {
 
-    const { login } = useContext(AuthContext);
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
 
-    const handleLogin = () => {
-        login();
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setInputs(prev => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs);
+      navigate("/")
+    } catch (err) {
+      setError(err.response.data)
     }
-    return (
-        <div className="login">
-            <div className="card">
-                <div className="left">
-                    <h1>Fake book</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, dolorem? Fugit necessitatibus, excepturi quae praesentium assumenda dolorum, deleniti quisquam magnam harum ipsa ipsam aperiam, minus ut nulla. Unde, at vero!</p>
-                    <span>Don't you have an account? </span>
-                    <Link to="/register">
-                        <button>Register</button>
-                    </Link>
-                </div>
-                <div className="right">
-                    <h1>Login</h1>
-                    <form action="">
-                        <input type="text" placeholder="Username" />
-                        <input type="password" placeholder="Password" />
-                        <button type="submit" onClick={handleLogin}>Login</button>
-                    </form>
-                </div>
-            </div>
+  }
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Fake book</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, dolorem? Fugit necessitatibus, excepturi quae praesentium assumenda dolorum, deleniti quisquam magnam harum ipsa ipsam aperiam, minus ut nulla. Unde, at vero!</p>
+          <span>Don't you have an account? </span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
         </div>
-    )
+        <div className="right">
+          <h1>Login</h1>
+          <form action="">
+            <input type="text" placeholder="Username" name="username" onChange={handleChange} />
+            <input type="password" placeholder="Password" name="password" onChange={handleChange} />
+            <button type="submit" onClick={handleLogin}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
